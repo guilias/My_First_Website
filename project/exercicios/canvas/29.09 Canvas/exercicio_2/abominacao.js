1c096d7e742b6a09a71d257e4e648e065730308f
guilias <guilhermeeliasb@gmail.com>
2025-10-08 17:18:22 -0300

    commiting

---
+++ project/exercicios/canvas/29.09 Canvas/exercicio_2/abominacao.js
@@ -0,0 +1,10 @@
+let canvas = document.getElementById('my-canvas');
+let ctx = canvas.getContext('2d')
+
+//mexi em alguns tamanhos pra deixar mais bonitinho T.T foi mal
+
+ctx.beginPath();
+ctx.fillStyle = 'gray';
+ctx.fillRect(0, 350, 400, 50);
+ctx.closePath();
+
